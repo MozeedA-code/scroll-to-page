ff7d6c5395003cb0045014b8094ece68d1a68dac
Mozeed Arogundade <mozeedarogundade02@gmail.com>
2025-05-22 11:50:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function homeClick() {
+  const homeSection = document.getElementById("home-sectionID");
+  homeSection.scrollIntoView("show");
+}
+
+function aboutClick() {
+  const aboutSection = document.getElementById("aboutPage");
+  aboutSection.scrollIntoView("show");
+}
+
+function serviceID() {
+  const serviceSection = document.getElementById("serviceID");
+  serviceSection.scrollIntoView("show");
+}
+function myskillID() {
+  const myskiilSection = document.getElementById("myskillID");
+  myskiilSection.scrollIntoView("show");
+}
+
+function contactID() {
+  const contactSection = document.getElementById("contactID");
+  contactSection.scrollIntoView("show");
+}
